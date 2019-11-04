9b59b64be9837db81af375963943a3c84da876d6
ugo119 <onuoha.ugochukwu@yahoo.com>
2019-11-04 11:03:59 +0100

    first commit for fragment concept

---
+++ Android-Me-master-ugo/app/src/main/java/com/example/ugo/android_me/ui/AndroidMeActivity.java
@@ -0,0 +1,19 @@
+package com.example.ugo.android_me.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ugo.android_me.R;
+
+// This activity will display a custom Android image composed of three body parts: head, body, and legs
+public class AndroidMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+
+
+        }
+    }
+
---
+++ Android-Me-master-ugo/app/src/main/java/com/example/ugo/android_me/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ugo.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ugo.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Variables to store the values for the list index of the selected images, default value will be 0
+    private int headIndex, bodyIndex, legsIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    }
+
